6f9a233f925354655f702b45e055206533203318
Duarte Branco <duartebranco35@gmail.com>
2023-12-07 17:18:29 +0000

    fi

---
+++ NBA/playersDetails.js
@@ -0,0 +1,135 @@
+console.log(localStorage.getItem('theme'));
+
+// Add event listener
+$('.switch').click(function() {
+    if ($('html').attr('data-bs-theme') == 'dark') {
+        $('html').removeAttr('data-bs-theme');
+        localStorage.removeItem('theme');
+        console.log(localStorage.getItem('theme'));
+        return;
+    } else {
+        $('html').attr('data-bs-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        console.log(localStorage.getItem('theme'));
+        return;
+    }
+});
+
+// On page load, check local storage for theme preference
+$(window).on('load', function() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        $('html').attr('data-bs-theme', 'dark');
+    } 
+});
+
+
+// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/NBA/API/Players/');
+    self.displayName = 'NBA Players Details';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    //--- Data Record
+    self.Id = ko.observable('');
+    self.Name = ko.observable('');
+    self.Birthdate = ko.observable('');
+    self.CountryId = ko.observable('');
+    self.CountryName = ko.observable('');
+    self.DraftYear = ko.observable('');
+    self.PositionId = ko.observable('');
+    self.PositionName = ko.observable('');
+    self.Height = ko.observable('');
+    self.Weight = ko.observable('');
+    self.School = ko.observable('');
+    self.Biography = ko.observable('');
+
+    //--- Page Events
+    self.activate = function (id) {
+        console.log('CALL: getArena...');
+        var composedUri = self.baseUri() + id;
+        ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            hideLoading();
+            self.Id(data.Id);
+            self.Name(data.Name);
+            self.Birthdate(data.Birthdate);
+            self.CountryId(data.CountryId);
+            self.CountryName(data.CountryName);
+            self.DraftYear(data.DraftYear);
+            self.PositionId(data.PositionId);
+            self.PositionName(data.PositionName);
+            self.Height(data.Height);
+            self.Weight(data.Weight);
+            self.School(data.School);
+            self.Biography(data.Biography);
+        });
+    };
+
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+    }
+
+    function showLoading() {
+        $('#myModal').modal('show', {
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        })
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
+
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('id');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+    console.log("VM initialized!");
+};
+
+$(document).ready(function () {
+    console.log("document.ready!");
+    ko.applyBindings(new vm());
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
