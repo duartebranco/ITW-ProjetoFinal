be53d09cfe6da400d193550848e8e9327b1adba1
NearGM <duartebranco35@gmail.com>
2024-01-27 01:52:24 +0000

    add NBA

---
+++ NBA/myJS.js
@@ -0,0 +1,40 @@
+console.log(localStorage.getItem('theme'));
+
+// Add event listener
+$('.switch').click(function() {
+    if ($('html').attr('data-bs-theme') == 'dark') {
+
+        $('html').removeAttr('data-bs-theme');
+        localStorage.removeItem('theme');
+        console.log(localStorage.getItem('theme'));
+        $(this).children('i').removeClass('fa-toggle-off').addClass('fa-toggle-on');
+        return;
+    } else {
+        $('html').attr('data-bs-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        console.log(localStorage.getItem('theme'));
+        $(this).children('i').removeClass('fa-toggle-on').addClass('fa-toggle-off');
+        return;
+    }
+});
+
+// On page load, check local storage for theme preference
+$(window).on('load', function() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        $('html').attr('data-bs-theme', 'dark');
+        // Change the icon to 'fa-toggle-off' when the theme is 'dark'
+        $('.switch').children('i').removeClass('fa-toggle-on').addClass('fa-toggle-off');
+    } else {
+        // Change the icon to 'fa-toggle-on' when the theme is not 'dark'
+        $('.switch').children('i').removeClass('fa-toggle-off').addClass('fa-toggle-on');
+    }
+});
+
+$('.form-inline').on('submit', function(e) {
+    e.preventDefault();
+    var inputValue = $('.custom-search').val();
+    console.log(inputValue);
+    localStorage.setItem('search', inputValue);
+    window.location.href = 'search.html';
+});
